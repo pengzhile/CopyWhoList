61c99c34e1b5188433017c8ffc27bae66ee7dd5c
mm2236818 <65822249+mm2236818@users.noreply.github.com>
2020-05-24 00:36:30 +0800

    Update Main.lua
    增加了查询功能，提前设置要查询的关键词，不断点按钮会自动根据预设关键词按顺序查询；
    查询结果如果符合关键词则获取名单写入编辑框，不符合则重新查询，每个关键词只查询8次，无结果则跳过。

--- Main.lua
+++ Main.lua
@@ -1,4 +1,90 @@
 local ADDON_NAME = ...;
+local Whotxt = ""
+local Who = {}
+local WhoNo = 1
+local Queries = 0
+local WhoTime = time()
+if UnitFactionGroup("player") == "Alliance" then
+	Who = {
+-------------------联盟查询关键词-------------------
+--格式参考："地名","职业","种族","等级下限-等级上限"
+	{"玛拉顿","法师","人类","60-60"},
+	{"玛拉顿","法师","侏儒","60-60"},
+	{"祖尔法拉克","法师","人类","60-60"},
+	{"祖尔法拉克","法师","侏儒","60-60"},
+	{"奥达曼","法师","人类","60-60"},
+	{"奥达曼","法师","侏儒","60-60"},
+	{"阿塔哈卡神庙","法师","人类","60-60"},
+	{"阿塔哈卡神庙","法师","侏儒","60-60"},
+	{"黑石塔","法师","人类","60-60"},
+	{"黑石塔","法师","侏儒","60-60"},
+	{"斯坦索姆","法师","人类","60-60"},
+	{"斯坦索姆","法师","侏儒","60-60"},
+	{"提瑞斯法林地","","","1-20"},
+	{"凄凉之地","","","1-20"},
+	{"厄运之槌","","","1-20"},
+	{"东瘟疫之地","","","1-20"},
+	{"黑石山","","","1-20"},
+	{"荆棘谷","","","1-20"},
+	{"冬泉谷","","","1-20"},
+	{"塔纳利斯","","","1-20"},
+	{"希利苏斯","","","1-20"},
+	{"暴风城","战士","","1-1"},
+	{"暴风城","圣骑士","","1-1"},
+	{"暴风城","猎人","","1-1"},
+	{"暴风城","潜行者","","1-1"},
+	{"暴风城","德鲁伊","","1-1"},
+	{"暴风城","法师","","1-1"},
+	{"暴风城","术士","","1-1"},
+	{"暴风城","牧师","","1-1"},
+	{"铁炉堡","战士","","1-1"},
+	{"铁炉堡","圣骑士","","1-1"},
+	{"铁炉堡","猎人","","1-1"},
+	{"铁炉堡","潜行者","","1-1"},
+	{"铁炉堡","德鲁伊","","1-1"},
+	{"铁炉堡","法师","","1-1"},
+	{"铁炉堡","术士","","1-1"},
+	{"铁炉堡","牧师","","1-1"},
+
+}
+elseif UnitFactionGroup("player") == "Horde" then
+	Who = {
+-------------------部落查询关键词-------------------
+--格式参考："地名","职业","种族","等级下限-等级上限"
+	{"玛拉顿","法师","亡灵","60-60"},
+	{"玛拉顿","法师","巨魔","60-60"},
+	{"祖尔法拉克","法师","亡灵","60-60"},
+	{"祖尔法拉克","法师","巨魔","60-60"},
+	{"奥达曼","法师","亡灵","60-60"},
+	{"奥达曼","法师","巨魔","60-60"},
+	{"阿塔哈卡神庙","法师","亡灵","60-60"},
+	{"阿塔哈卡神庙","法师","巨魔","60-60"},
+	{"黑石塔","法师","亡灵","60-60"},
+	{"黑石塔","法师","巨魔","60-60"},
+	{"斯坦索姆","法师","亡灵","60-60"},
+	{"斯坦索姆","法师","巨魔","60-60"},
+	{"凄凉之地","","","1-20"},
+	{"厄运之槌","","","1-20"},
+	{"东瘟疫之地","","","1-20"},
+	{"黑石山","","","1-20"},
+	{"荆棘谷","","","1-20"},
+	{"冬泉谷","","","1-20"},
+	{"塔纳利斯","","","1-20"},
+	{"希利苏斯","","","1-20"},
+	{"荒芜之地","","","1-20"},
+	{"悲伤沼泽","","","1-20"},
+	{"奥格瑞玛","战士","","1-1"},
+	{"奥格瑞玛","萨满祭司","","1-1"},
+	{"奥格瑞玛","猎人","","1-1"},
+	{"奥格瑞玛","潜行者","","1-1"},
+	{"奥格瑞玛","德鲁伊","","1-1"},
+	{"奥格瑞玛","法师","","1-1"},
+	{"奥格瑞玛","术士","","1-1"},
+	{"奥格瑞玛","牧师","","1-1"},
+
+}
+end
+--------------------------------------------------------
 
 CopyWhoListMixin = {};
 
@@ -44,30 +130,68 @@ function CopyWhoListMixin:OnAddOnLoaded(...)
 end
 
 function CopyWhoListMixin:OnWhoListUpdate(...)
-    local num = C_FriendList.GetNumWhoResults();
-
+    --local num = C_FriendList.GetNumWhoResults();
+   local num = 1
     WhoFrameCopyButton:SetEnabled(num and num > 0);
 end
 
 function CopyWhoListMixin:OnCopyListClick(...)
-    local num = C_FriendList.GetNumWhoResults();
-    if (num == nil or num <= 0) then
-        return ;
-    end
+	--如果当前关键词不存在，则停止查询
+	if (Who[WhoNo] == nil) then
+		CopyWhoListMixin:CopyWhoFrameShow(Whotxt);
+		print("所有预设关键词已查询完毕，请按CTRL+C复制查询结果")
+		return ;
+	end
 
-    local info;
-    local txt = "";
-    local realm = GetRealmName();
-    local playerName = GetUnitName("player");
-
-    for i = 1, num do
-        info = C_FriendList.GetWhoInfo(i);
-        if (info.fullName ~= playerName) then
-            txt = txt .. info.fullName .. "/" .. realm .. (i < num and "\n" or "");
-        end
-    end
-
-    CopyWhoListMixin:CopyWhoFrameShow(txt);
+	--获取查询结果
+	local num = C_FriendList.GetNumWhoResults()
+	--如果查询结果为0，则重新查询
+	if (num == nil or num <= 0) then
+		--关键词查询8次无结果则跳过
+		if Queries >= 8 then
+			WhoNo = WhoNo + 1
+			Queries = 0
+		end
+		--每次查询间隔1秒
+		if (WhoTime <= time() - 1) then
+			C_FriendList.SendWho('z-"'..Who[WhoNo][1]..'" '..'c-"'..Who[WhoNo][2]..'" '..'r-"'..Who[WhoNo][3]..'" '..Who[WhoNo][4])
+			Queries = Queries + 1
+			WhoTime = time()
+			print("查询关键词"..WhoNo.."："..Who[WhoNo][1].." "..Who[WhoNo][2].." "..Who[WhoNo][3].." "..Who[WhoNo][4].."，次数："..Queries..(Queries >= 8 and "（查询8次无结果则跳过）"or""))
+		end
+	else
+		--查询结果有延迟，判断查询结果是否符合关键词，如果符合则获取名单写入编辑框，并切换下一个关键词，如果不符合则重新查询
+		local info = C_FriendList.GetWhoInfo(1)
+		if (Who[WhoNo][1] == "" or info.area == Who[WhoNo][1]) and (Who[WhoNo][2] == "" or info.classStr == Who[WhoNo][2]) and (Who[WhoNo][3] == "" or info.raceStr == Who[WhoNo][3]) and (Who[WhoNo][4] == "" or (info.level >= tonumber(string.sub(Who[WhoNo][4],1,string.find(Who[WhoNo][4],"-")-1)) and info.level <= tonumber(string.sub(Who[WhoNo][4],string.find(Who[WhoNo][4],"-")+1,-1)))) then
+			local txt = "";
+			local realm = GetRealmName();
+			local playerName = GetUnitName("player");
+			for i = 1, num do
+				info = C_FriendList.GetWhoInfo(i);
+				if (info.fullName ~= playerName) then
+					txt = txt .. info.fullName .. "/" .. realm .. (i < num and "\n" or "");
+				end
+			end
+			Whotxt = Whotxt .. txt .. "\n\n"
+			WhoNo = WhoNo + 1
+			Queries = 0
+			CopyWhoListMixin:CopyWhoFrameShow(Whotxt);
+			print("查询成功：获取名单到编辑框")
+		else
+			--关键词查询8次无结果则跳过
+			if Queries >= 8 then
+				WhoNo = WhoNo + 1
+				Queries = 0
+			end
+			--每次查询间隔1秒
+			if (WhoTime <= time() - 1) then
+				C_FriendList.SendWho('z-"'..Who[WhoNo][1]..'" '..'c-"'..Who[WhoNo][2]..'" '..'r-"'..Who[WhoNo][3]..'" '..Who[WhoNo][4])
+				Queries = Queries + 1
+				WhoTime = time()
+				print("查询关键词"..WhoNo.."："..Who[WhoNo][1].." "..Who[WhoNo][2].." "..Who[WhoNo][3].." "..Who[WhoNo][4].."，次数："..Queries..(Queries >= 8 and "（查询8次无结果则跳过）"or""))
+			end
+		end
+	end
 end
 
 function CopyWhoListMixin:OnCopyBattleNamesClick(...)
